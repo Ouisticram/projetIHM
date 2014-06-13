0be318055f97252c9056019afc76d9d85966717b
Sheppard6 <kevin.briand@live.fr>
2014-06-13 17:29:31 +0200

    onglet Personnel (buggé)

--- projetIHM/src/view/NewContact.java
+++ projetIHM/src/view/NewContact.java
@@ -28,10 +28,12 @@ public class NewContact extends Container {
 		panel1.setLayout(new GridLayout(4,2));		
 		JComponent panel2 = new JPanel();
 		tabbedPane.addTab("personnel", panel2);
-		panel2.setLayout(new GridLayout(1, 1));
+		panel2.setLayout(new GridLayout(4,1));
 		JComponent panel3 = new JPanel();
 		tabbedPane.addTab("professionnel", panel3);
 		panel3.setLayout(new GridLayout(1, 1));
+
+		// ********************[ONGLET GENERAL]***********************
 
 		Box box1 = Box.createHorizontalBox();
 		JLabel nom = new JLabel("nom: ");
@@ -59,6 +61,69 @@ public class NewContact extends Container {
 		panel1.add(box1);
 		panel1.add(box2);
 		panel1.add(box3);
+
+
+// ***********************[ONGLET PERSONNEL]*******************
+
+
+		/*Box pers1 = Box.createHorizontalBox();
+		JLabel adresse = new JLabel("Adresse: ");
+		JTextField enterAdresse = new JTextField();
+		pers1.add(adresse);
+		pers1.add(enterAdresse);
+
+		Box pers2 = Box.createHorizontalBox();
+		JLabel telD = new JLabel("Téléphone Domicile : ");
+		JTextField enterTelD= new JTextField();
+		pers2.add(telD);
+		pers2.add(enterTelD);
+
+		Box pers3 = Box.createHorizontalBox();
+		JLabel telP = new JLabel("Téléphone Portable : ");
+		JTextField enterTelP= new JTextField();
+		pers2.add(telP);
+		pers2.add(enterTelP);
+
+		Box pers4 = Box.createHorizontalBox();
+		JLabel email = new JLabel("Email : ");
+		JTextField enterEmail = new JTextField();
+		pers2.add(email);
+		pers2.add(enterEmail);*/
+
+		JPanel pers1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JLabel adresse = new JLabel("Adresse: ");
+		JTextField enterAdresse = new JTextField(50);
+		pers1.add(adresse);
+		pers1.add(enterAdresse);
+
+		JPanel pers2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JLabel telD = new JLabel("Téléphone Domicile : ");
+		JTextField enterTelD= new JTextField(10);
+		pers2.add(telD);
+		pers2.add(enterTelD);
+
+
+		JPanel pers3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JLabel telP = new JLabel("Téléphone Portable : ");
+		JTextField enterTelP= new JTextField(10);
+		pers2.add(telP);
+		pers2.add(enterTelP);
+
+
+		JPanel pers4 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JLabel email = new JLabel("Email : ");
+		JTextField enterEmail = new JTextField(30);
+		pers2.add(email);
+		pers2.add(enterEmail);
+
+
+		panel2.add(pers1);
+		panel2.add(pers2);
+		panel2.add(pers3);
+		panel2.add(pers4);
+
+		// ******************************************************
+
 		
 		this.panel.setLayout(new BorderLayout());
 		this.panel.add(tabbedPane, BorderLayout.CENTER);
